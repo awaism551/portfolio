72c028769f1fb20d295c00d838805e62ba561c9f
Awais <awaism551@gmail.com>
2025-02-09 16:33:43 +0500

    Initial commit

---
+++ components/home/constants/skillIcons.tsx
@@ -0,0 +1,147 @@
+import {
+  SiHtml5,
+  SiCss3,
+  SiJavascript,
+  SiGit,
+  SiNodedotjs,
+  SiReact,
+  SiRedux,
+  SiMongodb,
+  SiJest,
+  SiMui,
+  SiNextdotjs,
+  SiPostgresql,
+  SiFirebase,
+  SiTailwindcss,
+  SiTypescript,
+  SiAmazonwebservices,
+  SiGooglecloud,
+  SiTerraform,
+  SiAngular,
+  SiNestjs,
+  SiExpress,
+  SiMysql,
+  SiIonic,
+  SiGraphql,
+  SiTypeorm,
+  SiSequelize,
+  SiRxdb,
+  SiPython,
+} from 'react-icons/si';
+
+const skillLogos = [
+  {
+    icon: <SiHtml5 title="Html5" />,
+    label: 'Html5',
+  },
+  {
+    icon: <SiCss3 title="CSS3" />,
+    label: 'CSS3',
+  },
+  {
+    icon: <SiJavascript title="JavaScript" />,
+    label: 'JavaScript',
+  },
+  {
+    icon: <SiTypescript title="TypeScript" />,
+    label: 'TypeScript',
+  },
+  // {
+  //   icon: <SiGit title="Git" />,
+  //   label: 'Git',
+  // },
+  {
+    icon: <SiReact title="React" />,
+    label: 'React',
+  },
+  {
+    icon: <SiNextdotjs title="NextJS" />,
+    label: 'NextJS',
+  },
+  {
+    icon: <SiAngular title="Angular" />,
+    label: 'Angular',
+  },
+  {
+    icon: <SiRedux title="Redux" />,
+    label: 'Redux',
+  },
+  {
+    icon: <SiRxdb title="Rxjs" />,
+    label: 'Rxjs',
+  },
+  {
+    icon: <SiGraphql title="GraphQL" />,
+    label: 'GraphQL',
+  },
+  {
+    icon: <SiNodedotjs title="Node" />,
+    label: 'Node',
+  },
+  {
+    icon: <SiNestjs title="NestJS" />,
+    label: 'NestJS',
+  },
+  {
+    icon: <SiExpress title="ExpressJS" />,
+    label: 'ExpressJS',
+  },
+  {
+    icon: <SiPython title="Python" />,
+    label: 'Python',
+  },
+  {
+    icon: <SiPostgresql title="PostgreSQL" />,
+    label: 'PostgreSQL',
+  },
+  {
+    icon: <SiMysql title="Mysql" />,
+    label: 'Mysql',
+  },
+  {
+    icon: <SiIonic title="Ionic" />,
+    label: 'Ionic',
+  },
+  {
+    icon: <SiMongodb title="MongoDB" />,
+    label: 'MongoDB',
+  },
+  {
+    icon: <SiFirebase title="Firebase" />,
+    label: 'Firebase',
+  },
+  {
+    icon: <SiJest title="Jest" />,
+    label: 'Jest',
+  },
+  // {
+  //   icon: <SiTailwindcss title="Tailwind CSS" />,
+  //   label: 'Tailwind CSS',
+  // },
+  {
+    icon: <SiMui title="Material UI" />,
+    label: 'Material UI',
+  },
+  {
+    icon: <SiTypeorm title="Typeorm" />,
+    label: 'Typeorm',
+  },
+  {
+    icon: <SiSequelize title="Sequelize" />,
+    label: 'Sequelize',
+  },
+  // {
+  //   icon: <SiTerraform title="Terraform" />,
+  //   label: 'Terraform',
+  // },
+  {
+    icon: <SiAmazonwebservices title="Amazon Web Services" />,
+    label: 'Amazon Web Services',
+  },
+  // {
+  //   icon: <SiGooglecloud title="Google Cloud" />,
+  //   label: 'Google Cloud',
+  // },
+];
+
+export default skillLogos;
