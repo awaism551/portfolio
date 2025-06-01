0740cec38e59741f8d8e038349d57e5107ff640b
Awais <awaism551@gmail.com>
2025-06-01 22:34:45 +0500

    updates in master

--- components/Link.tsx
+++ components/Link.tsx
@@ -54,7 +54,7 @@ export const NextLinkComposed = React.forwardRef<
 export type LinkProps = {
   activeClassName?: string;
   as?: NextLinkProps['as'];
-  href: NextLinkProps['href'];
+  href?: NextLinkProps['href'];
   linkAs?: NextLinkProps['as']; // Useful when the as prop is shallow by styled().
   noLinkStyle?: boolean;
 } & Omit<NextLinkComposedProps, 'to' | 'linkAs' | 'href'> &
@@ -85,6 +85,7 @@ const Link = React.forwardRef<HTMLAnchorElement, LinkProps>(function Link(
 
   const router = useRouter();
   console.log("🚀 ~ href:", href)
+  if (!href) return null;
   const pathname = typeof href === 'string' ? href : href?.pathname;
   const className = clsx(classNameProps, {
     [activeClassName]: router.pathname === pathname && activeClassName,
