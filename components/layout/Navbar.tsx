6f5506c5996b1a5be5ca7caee6a618a473d2017c
Awais <awaism551@gmail.com>
2025-06-03 14:36:10 +0500

    Update social icons, remove resume files, and disable language selector
    - Replaces Twitter icon with new icons for X and Linktree in social icons list.
    - Corrects text in English locale for improved readability.
    - Removes outdated resume files in JSON and PDF formats.
    - Temporarily disables the language selector in the navigation bar.

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -104,7 +104,7 @@ export default function ElevateAppBar() {
                 </AnimatedLink>
               ))}
 
-              <LanguageSelector />
+              {/* <LanguageSelector /> */}
 
               <IconButton
                 aria-label="Open Navigation"
